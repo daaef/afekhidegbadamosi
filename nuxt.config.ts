314a6a92fa99946d432ef34d0afb67a591db6cec
Afekhide Gbadamosi <realafe21@gmail.com>
2024-08-14 14:14:18 +0100

    added studio module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   extends: 'content-wind',
+
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+
+  compatibilityDate: '2024-08-14',
+  modules: ['@nuxthq/studio']
 })
